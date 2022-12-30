008d092f8aa1419658b8668ca2df9ea5277aba8f
Huseyin Can Ercan <huscanercan@gmail.com>
2022-12-30 09:54:20 +0300

    Async file operations

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -2,3 +2,25 @@ const fs =  require('fs');
 
 const hello = 'Hello world';
 console.log(hello);
+
+const textInput = fs.readFileSync('./txt/input.txt', 'utf8');
+console.log(textInput);
+
+const textOut = `Information about avocado: ${textInput}\nCreated on ${Date.now()}`;
+
+fs.writeFileSync('./txt/output.txt', textOut);
+
+
+//err, data parameter pattern is common on Node.js
+fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+    fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+        console.log(data2);
+        fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
+            console.log(data3);
+            fs.writeFile('./txt/final.txt', `${data2}\n${data3}`,'utf-8', (err) => {
+                console.log('Your file was successfully written');
+            });
+        });
+    });
+});
+console.log('Will read the file');
