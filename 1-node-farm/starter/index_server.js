a9a51ecd680b8e42681723a9849fe6a4904b8907
Huseyin Can Ercan <huscanercan@gmail.com>
2023-01-02 21:59:10 +0300

    Add template filling

--- 1-node-farm/starter/index_server.js
+++ 1-node-farm/starter/index_server.js
@@ -1,8 +1,37 @@
 const http = require("http");
 const fs = require("fs");
 
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  "utf-8"
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8"
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  "utf-8"
+);
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
-const dataObject = JSON.parse(data);
+const dataObj = JSON.parse(data);
+
+//initialize the replaceTemplate method
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+
+  if (!product.organic) {
+    output = output.replace(/{%ORGANIC%}/g, "not-organic");
+  }
+  return output;
+};
 
 const server = http.createServer((req, res) => {
   const pathName = req.url;
@@ -10,14 +39,18 @@ const server = http.createServer((req, res) => {
   switch (pathName) {
     case "/":
     case "/overview":
-      res.end("This is overview");
+      res.writeHead(200, { "Content-Type": "text/html" });
+      const cardsHtml = dataObj
+        .map((el) => replaceTemplate(tempCard, el))
+        .join("");
+      const output = tempOverview.replace(/{%PRODUCT_CARDS%}/g, cardsHtml);
+      res.end(output);
       break;
     case "/product":
-      res.end("This is product");
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(tempProduct);
       break;
     case "/api":
-      //fs.readFile('./dev-data/data.json' , (err, data) => {
-      //fs.readFile(`${__dirname}/dev-data/data.json`, "utf-8", (err, data) => {
       const productData = JSON.parse(data);
       res.writeHead(200, { "Content-Type": "application/json" });
       res.end(data);
