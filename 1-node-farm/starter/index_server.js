6bf211769cf9cfb7ae674611636d6eace9edb8d8
Huseyin Can Ercan <huscanercan@gmail.com>
2023-01-03 10:10:15 +0300

    Add package json

--- 1-node-farm/starter/index_server.js
+++ 1-node-farm/starter/index_server.js
@@ -1,6 +1,7 @@
 const http = require("http");
 const fs = require("fs");
 const url = require("url");
+const slugify = require('slugify');
 const replaceTemplate = require('./modules/replaceTemplate');
 
 const tempOverview = fs.readFileSync(
@@ -17,6 +18,10 @@ const tempProduct = fs.readFileSync(
 );
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
+
+console.log(slugify('Fresh Avocados', { lower: true }));
+const slugs = dataObj.map((el) => slugify(el.productName, { lower: true }));
+console.log(slugs);
 
 const server = http.createServer((req, res) => {
   const { query, pathname } = url.parse(req.url, true);
