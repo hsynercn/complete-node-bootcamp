023400093a5431909bbe8d6a378a0127664452e5
Huseyin Can Ercan <huscanercan@gmail.com>
2023-01-09 01:31:43 +0300

    Add async/await sample

--- 3-asynchronous-JS/starter/index.js
+++ 3-asynchronous-JS/starter/index.js
@@ -25,6 +25,8 @@ const writeFilePro = (file, data) => {
   });
 };
 
+//We have a better mechanism then 'then' expression
+/*
 readFilePro(`${__dirname}/dog.txt`)
   .then((data) => {
     console.log(`Breed: ${data}`);
@@ -40,3 +42,22 @@ readFilePro(`${__dirname}/dog.txt`)
   .catch((err) => {
     console.log(err);
   });
+*/
+
+//async: that is a special function, this code will run at the background without blocking the event loop
+const getDocPic = async () => {
+  try {
+    //this code will wait until read promise is resolved
+    const data = await readFilePro(`${__dirname}/dog.txt`);
+    console.log(`Breed: ${data}`);
+    const res = await superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
+    await writeFilePro('dog-img.txt', res.body.message);
+    console.log('Done');
+  } catch (error) {
+    console.log(err);
+  }
+};
+
+getDocPic();
