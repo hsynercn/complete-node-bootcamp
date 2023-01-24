1e1208ead0f1ee66c6346980554a51d289aa7665
Huseyin Can Ercan <huscanercan@gmail.com>
2023-01-24 08:57:53 +0300

    Add filtering

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -1,8 +1,29 @@
 const Tour = require('../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
+  console.log(req.query);
   try {
-    const tours = await Tour.find();
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((el) => delete queryObj[el]);
+
+    console.log(req.query, queryObj);
+
+    /*
+    const tours = await Tour.find({
+      duration: 5,
+      difficulty: 'easy',
+    });
+    */
+    /*
+    const tours = await Tour.find()
+      .where('duration')
+      .equals(5)
+      .where('difficulty')
+      .equals('easy');
+    */
+    const tours = await Tour.find(queryObj);
+
     res.status(200).json({
       status: 'success',
       requestedAt: req.requestTime,
