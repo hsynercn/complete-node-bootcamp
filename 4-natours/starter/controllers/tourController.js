ff1d3d1f612a0950edf61a92cab8aafd6878bbc3
Huseyin Can Ercan <huscanercan@gmail.com>
2023-01-24 17:02:00 +0300

    Add advanced filtering

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -7,7 +7,11 @@ exports.getAllTours = async (req, res) => {
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach((el) => delete queryObj[el]);
 
-    const query = Tour.find(queryObj);
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+    console.log(queryStr);
+
+    const query = Tour.find(JSON.parse(queryStr));
     const tours = await query;
 
     res.status(200).json({
