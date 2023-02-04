1d680457d3664722270500cc201e756045dad9b3
Huseyin Can Ercan <huscanercan@gmail.com>
2023-02-04 21:25:52 +0300

    Add dev prod differences

--- 4-natours/starter/controllers/errorController.js
+++ 4-natours/starter/controllers/errorController.js
@@ -1,8 +1,38 @@
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  if (err.isOperational) {
+    return res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  }
+  console.error('ERROR', err);
+  //Programming or other unknown error: don't leak error details
+  return res.status(500).json({
+    status: 'error',
+    message: 'Something went wrong',
+  });
+};
+
 const errorHandler = (err, req, res, next) => {
   console.error(err.stack);
 
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
+
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProd(err, res);
+  }
 
   res.status(err.statusCode).json({
     status: err.status,
