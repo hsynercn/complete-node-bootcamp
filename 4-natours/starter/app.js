527cd82f12ebc850fcab53c8f158cc03d38b24e3
Huseyin Can Ercan <huscanercan@gmail.com>
2023-02-04 10:18:37 +0300

    Refactor error and error handler

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -3,6 +3,8 @@ const morgan = require('morgan');
 
 const app = express();
 
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -24,23 +26,10 @@ app.use('/api/v1/users', userRouter);
 
 //if we are here, it means that the route is not defined
 app.all('*', (req, res, next) => {
-  const err = new Error(`Can't find ${req.originalUrl} on this server!`);
-  err.status = 'fail';
-  err.statusCode = 404;
-
-  //passing the error to the next middleware, express will know that this is an error
-  next(err);
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
 //four arguments are required for error handling middleware, express will know that this is an error handling middleware
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
-
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
-});
+app.use(globalErrorHandler);
 
 module.exports = app;
