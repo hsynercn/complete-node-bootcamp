d6d68cfd5068809e4819135ed6f9f5ce62d892b8
Huseyin Can Ercan <huscanercan@gmail.com>
2023-02-05 10:56:31 +0300

    Add uncaught exception handler

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -5,6 +5,12 @@ dotenv.config({ path: './config.env' });
 
 const app = require('./app');
 
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION! Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
   process.env.DATABASE_PASSWORD
@@ -27,8 +33,8 @@ const server = app.listen(port, () => {
 
 //safety net for unhandled rejections
 process.on('unhandledRejection', (err) => {
-  console.log(err.name, err.message);
   console.log('UNHANDLED REJECTION! Shutting down...');
+  console.log(err.name, err.message);
   server.close(() => {
     process.exit(1);
   });
