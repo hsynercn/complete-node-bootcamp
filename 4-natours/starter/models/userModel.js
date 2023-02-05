ffbb4e15cdd9eaada46a9d1001e8b845c90c3412
Huseyin Can Ercan <huscanercan@gmail.com>
2023-02-06 00:01:58 +0300

    Hashing for passwords

--- 4-natours/starter/models/userModel.js
+++ 4-natours/starter/models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -25,6 +26,10 @@ const userSchema = new mongoose.Schema({
   password: {
     type: String,
     required: [true, 'A user must have a password'],
+    maxlength: [
+      40,
+      'A user password confirmation must have less or equal then 40 characters',
+    ],
     minlength: [
       10,
       'A user password must have more or equal then 10 characters',
@@ -42,7 +47,21 @@ const userSchema = new mongoose.Schema({
       10,
       'A user password confirmation must have more or equal then 10 characters',
     ],
+    validate: {
+      // this only works with SAVE, when we create a new object
+      validator: function (element) {
+        return element === this.password;
+      },
+      message: 'Passwords are not the same',
+    },
   },
+});
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 12);
+  this.passwordConfirm = undefined;
+  next();
 });
 
 const User = mongoose.model('User', userSchema);
