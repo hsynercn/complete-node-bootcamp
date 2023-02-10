71bf90129943d98571c823c24edd9b9208152d36
Huseyin Can Ercan <huscanercan@gmail.com>
2023-02-10 09:44:35 +0300

    Login endpoint with JWT compare

--- 4-natours/starter/controllers/authController.js
+++ 4-natours/starter/controllers/authController.js
@@ -1,6 +1,13 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
+
+function signToken(id) {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+}
 
 exports.signup = catchAsync(async (req, res, next) => {
   //safe implementation
@@ -13,11 +20,7 @@ exports.signup = catchAsync(async (req, res, next) => {
 
   //we can pass some options
 
-  console.log(`>>>${process.env.JWT_TOKEN}`);
-
-  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN,
-  });
+  const token = signToken(newUser._id);
 
   res.status(201).json({
     status: 'success',
@@ -25,5 +28,30 @@ exports.signup = catchAsync(async (req, res, next) => {
     data: {
       user: newUser,
     },
+  });
+});
+
+exports.login = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return next(new AppError('Please provide email and password', 400));
+  }
+
+  const user = await User.findOne({ email }).select('+password');
+  console.log('>>>>user', user);
+  let correct = false;
+  if (user) {
+    correct = await user.correctPassword(password, user.password);
+  }
+  if (!user || !correct) {
+    return next(new AppError('Incorrect email or password', 401));
+  }
+
+  const token = signToken(user._id);
+
+  res.status(200).json({
+    status: 'success',
+    token,
   });
 });
--- 4-natours/starter/models/userModel.js
+++ 4-natours/starter/models/userModel.js
@@ -26,6 +26,7 @@ const userSchema = new mongoose.Schema({
   password: {
     type: String,
     required: [true, 'A user must have a password'],
+    select: false,
     maxlength: [
       40,
       'A user password confirmation must have less or equal then 40 characters',
@@ -39,6 +40,7 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, 'A user must have a password confirmation'],
     trim: true,
+    select: false,
     maxlength: [
       40,
       'A user password confirmation must have less or equal then 40 characters',
@@ -63,6 +65,13 @@ userSchema.pre('save', async function (next) {
   this.passwordConfirm = undefined;
   next();
 });
+
+userSchema.methods.correctPassword = async function (
+  candidatePassword,
+  userPassword
+) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
 
 const User = mongoose.model('User', userSchema);
 module.exports = User;
