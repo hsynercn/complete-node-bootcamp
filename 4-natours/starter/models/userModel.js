8cd1597b7e9b60072b745ac19dfa7c53c22c6f73
Huseyin Can Ercan <huscanercan@gmail.com>
2023-02-13 11:31:55 +0300

    Improve protection

--- 4-natours/starter/models/userModel.js
+++ 4-natours/starter/models/userModel.js
@@ -57,6 +57,7 @@ const userSchema = new mongoose.Schema({
       message: 'Passwords are not the same',
     },
   },
+  passwordChangedAt: Date,
 });
 
 userSchema.pre('save', async function (next) {
@@ -71,6 +72,16 @@ userSchema.methods.correctPassword = async function (
   userPassword
 ) {
   return await bcrypt.compare(candidatePassword, userPassword);
+};
+
+userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
+  if (this.passwordChangedAt) {
+    const changedTimestamp = this.passwordChanged.getTime();
+    console.log('changedTimestamp: ', changedTimestamp);
+    console.log('JWTTimestamp: ', JWTTimestamp);
+    return JWTTimestamp < changedTimestamp;
+  }
+  return false;
 };
 
 const User = mongoose.model('User', userSchema);
